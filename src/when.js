d3f87dd7e24a9a37a15815a9a5067b3d328a474e
Miguel Castillo <manchagnu@gmail.com>
2014-07-29 23:46:51 -0400

    New logic in When using All

---
+++ src/when.js
@@ -0,0 +1,30 @@
+/**
+ * spromise Copyright (c) 2014 Miguel Castillo.
+ * Licensed under MIT
+ */
+
+
+define([
+  "src/promise",
+  "src/all"
+], function(Promise, All) {
+  "use strict";
+
+  /**
+  * Interface to allow multiple promises to be synchronized
+  */
+  function When() {
+    var context = this, args = arguments;
+    return Promise(function(resolve, reject) {
+      All.call(context, args).then(function(results) {
+        resolve.apply(context, results);
+      },
+      function(reason) {
+        reject.call(context, reason);
+      });
+    });
+  }
+
+  return When;
+});
+
