939984f55a1f4e80b22690d6dba1505753ce72b1
Miguel Castillo <manchagnu@gmail.com>
2016-03-12 14:49:33 -0500

    Added polyfill helper to automatically hook into the global object when the polyfill method is called

--- src/spromise.js
+++ src/spromise.js
@@ -12,5 +12,16 @@
   Promise.all   = require("./all");
   Promise.race  = require("./race");
 
+  Promise.polyfill = function(force) {
+    if (typeof(window) !== "undefined" && (!window.hasOwnProperty("Promise") || force !== false)) {
+      window.Promise = Promise;
+    }
+
+    if (typeof(global) !== "undefined" && (!global.hasOwnProperty("Promise") || force !== false)) {
+      global.Promise = Promise;
+    }
+  };
+
+  Promise.polyfill(false);
   module.exports = Promise;
 }());
